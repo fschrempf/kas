e6131acec7d1d4aafd50d7acac347e2b25912dcd
Frieder Schrempf <frieder.schrempf@kontron.de>
2024-10-29 17:47:08 +0100

    tests: includehandler: Extend tests to check parsed config file list
    Extend the available tests for include files to check if the files
    are present in the new config file list and if the expected flags
    are set.
    
    Signed-off-by: Frieder Schrempf <frieder.schrempf@kontron.de>
    Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

--- tests/test_includehandler.py
+++ tests/test_includehandler.py
@@ -149,6 +149,13 @@ header:
                 assert test['conf'] == config
                 assert test['rmiss'] == missing
 
+                if 'cfiles' in test:
+                    assert len(ginc.config_files) == len(test['cfiles'])
+
+                    for i, cf in enumerate(ginc.config_files):
+                        assert test['cfiles'][i][0] == cf.filename
+                        assert test['cfiles'][i][1] == cf.is_lockfile
+
     def test_valid_includes_none(self, monkeypatch):
         header = self.__class__.header
         testvector = [
@@ -161,6 +168,12 @@ header:
                 'conf': {
                 },
                 'rmiss': [
+                ],
+                'cfiles': [
+                    (
+                        'x.yml',
+                        False
+                    )
                 ]
             },
         ]
@@ -182,6 +195,16 @@ header:
                     'v': None
                 },
                 'rmiss': [
+                ],
+                'cfiles': [
+                    (
+                        os.path.abspath('y.yml'),
+                        False,
+                    ),
+                    (
+                        'x.yml',
+                        False,
+                    )
                 ]
             },
             # Include one file from another not available repo:
@@ -196,6 +219,12 @@ header:
                 },
                 'rmiss': [
                     'rep',
+                ],
+                'cfiles': [
+                    (
+                        'x.yml',
+                        False,
+                    )
                 ]
             },
             # Include one file from the same repo and one from another
@@ -213,6 +242,16 @@ header:
                 },
                 'rmiss': [
                     'rep',
+                ],
+                'cfiles': [
+                    (
+                        os.path.abspath('y.yml'),
+                        False
+                    ),
+                    (
+                        'x.yml',
+                        False
+                    )
                 ]
             },
             # Include one file from another available repo:
@@ -229,6 +268,16 @@ header:
                     'v': None
                 },
                 'rmiss': [
+                ],
+                'cfiles': [
+                    (
+                        '/rep/y.yml',
+                        False
+                    ),
+                    (
+                        'x.yml',
+                        False
+                    )
                 ]
             },
             # Include two files from another repo in sub-directories:
@@ -247,6 +296,20 @@ header:
                     'v': None
                 },
                 'rmiss': [
+                ],
+                'cfiles': [
+                    (
+                        '/rep/dir2/z.yml',
+                        False
+                    ),
+                    (
+                        '/rep/dir1/y.yml',
+                        False
+                    ),
+                    (
+                        'x.yml',
+                        False
+                    )
                 ]
             },
         ]
